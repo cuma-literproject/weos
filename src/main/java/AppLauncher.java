c580087aa67e738b795266339d6f360c46aeeb93
myeongjin-shin <myeongjin.shin@linecorp.com>
2018-03-07 15:56:51 +0900

    Add spring boot skeleton project

---
+++ src/main/java/AppLauncher.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.SpringApplication;
+
+public class AppLauncher {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(AppLauncher.class, args);
+    }
+}
+

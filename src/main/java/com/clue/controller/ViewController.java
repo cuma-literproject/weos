8bacb0424b501ea3368c7472b036c8100657d97e
myeongjin.shin <myeongjin.shin@linecorp.com>
2018-03-09 13:59:44 +0900

    Update wallet page

--- src/main/java/com/clue/controller/ViewController.java
+++ src/main/java/com/clue/controller/ViewController.java
@@ -14,6 +14,6 @@ public class ViewController {
 
     @RequestMapping("/{server}/wallet")
     String wallet(@PathVariable String server, Model model) {
-        return "wallet";
+        return "home";
     }
 }

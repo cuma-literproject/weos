36346bb7f7775b73c2cb99631c303868d736aa20
myeongjin.shin <myeongjin.shin@linecorp.com>
2018-03-13 18:50:23 +0900

    Add get account api

--- src/main/java/com/clue/controller/ApiController.java
+++ src/main/java/com/clue/controller/ApiController.java
@@ -2,6 +2,7 @@ package com.clue.controller;
 
 import com.clue.crypto.ec.EosPrivateKey;
 import com.clue.model.EosChainInfo;
+import com.clue.remote.api.AccountInfoRequest;
 import com.clue.remote.api.Message;
 import com.clue.remote.api.PushTxnResponse;
 import com.clue.remote.chain.GetRequiredKeys;
@@ -13,6 +14,7 @@ import com.clue.util.Consts;
 import com.clue.util.RetrofitClient;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import org.springframework.ui.Model;
@@ -58,6 +60,47 @@ public class ApiController {
         return result;
     }
 
+    @RequestMapping("/{net}/get/account/{name}")
+    String getAccount(@PathVariable String net, @PathVariable String name, Model model) throws IOException {
+        EosdApi eosdApi = getEosdApi(net);
+
+        AccountInfoRequest req = new AccountInfoRequest(name);
+        Response<JsonObject> response = eosdApi.getAccountInfo(req).execute();
+        return response.body().toString();
+    }
+
+    private EosdApi getEosdApi(String net) {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(logging);
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(getDomain(net))
+                .client(httpClient.build())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        return retrofit.create(EosdApi.class);
+    }
+
+    private String getDomain(String net) {
+        if (net.equals("testnet1")) {
+            return "http://testnet1.eos.io";
+        }
+
+        if (net.equals("testnet2")) {
+            return "http://testnet2.eos.io";
+        }
+
+        return "http://localhost:8888";
+    }
+
     @RequestMapping("/{net}/new/account/{name}/{ownerKey}/{activeKey}")
     String register(@PathVariable String net, @PathVariable String name, @PathVariable String ownerKey, @PathVariable String activeKey, Model model) throws IOException {
         EosChainInfo info = getChainInfo().execute().body();
@@ -73,47 +116,34 @@ public class ApiController {
         keys2.add(realOwnerKey.getPublicKey().toString());
         keys2.add(realActiveKey.getPublicKey().toString());
 
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
         EosNewAccount newAccountData = new EosNewAccount(creator, name, keys.get(0), keys.get(1), creator);
+        System.out.println(gson.toJson(newAccountData).toString());
 
         SignedTransaction txn = createTransaction(Consts.EOS_CONTRACT_NAME, newAccountData.getTypeName(), newAccountData.getAsHex()
                 , new String[]{ newAccountData.getCreatorName(),Consts.EOS_CONTRACT_NAME }
                 , getActivePermission( newAccountData.getCreatorName() ), info );
 
 
+        EosdApi eosdApi = getEosdApi(net);
+        GetRequiredKeys getRequiredKeys = new GetRequiredKeys(txn, keys2);
+        System.out.println(gson.toJson(getRequiredKeys).toString());
 
-        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
-
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-        httpClient.addInterceptor(logging);
-
-
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://localhost:8888")
-                .client(httpClient.build())
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-
-        EosdApi eosdApi = retrofit.create(EosdApi.class);
-        Response<RequiredKeysResponse> response = eosdApi.getRequiredKeys(new GetRequiredKeys(txn, keys2)).execute();
+        Response<RequiredKeysResponse> response = eosdApi.getRequiredKeys(getRequiredKeys).execute();
 
         // no-key return
         // signTransaction(txn);
-
-        EosdApi eosdApi2 = retrofit.create(EosdApi.class);
+        EosdApi eosdApi2 = getEosdApi(net);
         Response<PushTxnResponse> response2 = eosdApi2.pushTransaction(txn).execute();
-
 
         String res = gson.toJson(response2.body()).toString();
         System.out.println(res);
-
-
         return res;
     }
+
 
     String decodeUTF8(byte[] bytes) {
         return new String(bytes, UTF8_CHARSET);
--- src/main/java/com/clue/controller/EosdApi.java
+++ src/main/java/com/clue/controller/EosdApi.java
@@ -1,10 +1,12 @@
 package com.clue.controller;
 
 import com.clue.model.EosChainInfo;
+import com.clue.remote.api.AccountInfoRequest;
 import com.clue.remote.api.PushTxnResponse;
 import com.clue.remote.chain.GetRequiredKeys;
 import com.clue.remote.chain.RequiredKeysResponse;
 import com.clue.remote.chain.SignedTransaction;
+import com.google.gson.JsonObject;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -20,4 +22,7 @@ public interface EosdApi {
 
     @POST("/v1/chain/push_transaction")
     Call<PushTxnResponse> pushTransaction(@Body SignedTransaction body);
+
+    @POST("/v1/chain/get_account")
+    Call<JsonObject> getAccountInfo(@Body AccountInfoRequest body);
 }
